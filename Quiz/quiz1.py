9c3eee1daa7891f0d8b76541bb088e0c5d4feded
MGarrett14 <144712229+MGarrett14@users.noreply.github.com>
2023-09-14 17:01:17 +0000

    done

--- Quiz/quiz1.py
+++ Quiz/quiz1.py
@@ -1,11 +1,19 @@
 # 1. In your on words, describe what a variable is.
-
+A; variable is a container for different values you put into a prograam
 # 2. Name three (3) operators we've learned about in class and describe 
+Three operators we've learned in class is mathmatic operator-symbols used to allow programmmers
+to perform math operations, Assignment operator- symbols used to assian a variable to a value,
+and Comparison operator- symbols used to compare values
 # a situation where you would use each one. 
+you would used the mathmatic operator when making a math problem, a assignment operator to assign
+a variable like Numberofcarrots=22, you would use a comparion operator to see if a variable is larger
+than the other like 24>2
 
 # 3. Describe the difference between camal case and pascal case.
-
+The difference between camal case and pascal case is that, camal case you capitalize every new word,
+but the first one, and pascal case is the exact same except you can capitalize the first word
 # 4. Which operator is being used in the example below
+The operator that is being used in the example below is comparison operator
 numberOfApples = 10
 numberOfOranges = 12
 
@@ -15,15 +23,23 @@ numberOfApples < numberOfOranges
 # family member adds 50.00 dollars to your gift card and a friend gives you $35.50. 
 # Your task is to create variables for each gift amount and a final variable that
 # represents the total amount of money on your gift card. 
+GiftCardFromAmountFamilyMember= 100
+GiftCardFromAmountAnotherFamilyMember= 50
+GiftCardAmountFromFriend= 35.50
+TotalAmountFromAllGiftCards= 185.50
 
 # 6. Create a variable that uses the input function and then print that variable. Your variable 
 # should ask a user to enter their name.  
+NameofPerson = input('What is your name?')
 
 # 7. You have been asked to create a program for a community center that wants to plan a cookout. 
 # The community center wants you to build a form program and capture several key data points. 
 # They want to know a visitor's full name, their age, if they have an allergy, and what their 
 # favorite food is. You need to create the appropriate varialbes with the appropriate data type
 # and use the input() function and print function to take in and print out the user's responses. 
+FullNameofVisitors = input('What is your full name?')
+AgeofVisitors = input('How old are you?')
+AllergiesofVisitors
 
 # 8. You have been given a job to create a program for a sandwich shop. The sandwich shop
 # wants to be able to know if a user has a gluten allegery before they make the sandwich order.

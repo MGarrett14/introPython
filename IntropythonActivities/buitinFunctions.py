100a701bc07dd37a0a26eca46da2a764546bf206
MGarrett14 <144712229+MGarrett14@users.noreply.github.com>
2023-09-19 16:53:50 +0000

    clsss work

---
+++ IntropythonActivities/buitinFunctions.py
@@ -0,0 +1,3 @@
+name = 'Mekhi Garrett'
+
+type(name)
---
+++ IntropythonActivities/readingActivities.py
@@ -0,0 +1,6 @@
+# read the following articles onPython functions
+https://www.freecodecamp.org/news/functions-in-python-a-beginners-guide
+
+
+# In your own words, describe what a function?
+def functionName()

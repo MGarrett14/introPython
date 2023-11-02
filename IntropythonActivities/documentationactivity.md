7c470956a8e4e6e1a7802e38b60a7583b641eba2
MGarrett14 <144712229+MGarrett14@users.noreply.github.com>
2023-11-02 16:53:42 +0000

    gitclone Assignment done

---
+++ IntropythonActivities/documentationactivity.md
@@ -0,0 +1,10 @@
+## Name of project
+Template of a good README documentation
+
+# The Engineers working on this project
+Ian kimble
+James Anthony-Version control
+Mekhi Garrett 
+and the leader of this team Frank Bar
+Link to the project- https://github.com/codespaces?repository_id=691161648
+
---
+++ cloneAssignment/mekhi_gitCloneFunction.py
@@ -0,0 +1,34 @@
+def period_Ending():
+     time_in_class= 1.30
+     time_class_is_over= 1.00
+
+if
+    time_in_class > time_class_is_over:
+    print('Class is over! Time to go')
+
+else:
+        print('class is still in session')
+
+period_Ending()
+
+
+# Name of Project
+'Clone github condition statement'
+ 
+ # Engineers of this project
+ 'Mekhi Garrett'
+
+# Description/ features
+ 'My program is a conditional statement that tells the programmer if the time in class is greater than the time when class is over'
+ 'The  class is over, but if the time in class less than it will tell the programmer class is not over' 
+ 'The Operator that is being used in this program is a comparsion operator, specfically the greather than(>) operator'
+
+# Installtion
+'You can install this program by going into my repository with the link I am inputting'
+https://github.com/codespaces?repository_id=691161648
+
+# Contribute
+' You can contribute to this program by using this code to make your own block of code'
+
+
+

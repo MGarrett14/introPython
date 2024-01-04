caf33bd6b64708f41e37ddd4b16bcd0965931114
MGarrett14 <144712229+MGarrett14@users.noreply.github.com>
2024-01-04 17:49:23 +0000

    Daily Activity finished

---
+++ LastPythonUnit.py/PythonReview.py
@@ -0,0 +1,31 @@
+'A computer program is a set of instructions on how to work/function a program in a wbesite or video game'
+
+'1 data type is a string, which is a set of words youre able to tpye sets of words in a program' 
+'You would use a string when typing a sentence in a program like saying how are you?'
+
+'Another data type is float which is a decimal number and you would use a float when typing out an amount of money'
+
+'Another data type is a boolean, which is used for true or false statement  '
+
+
+
+
+
+
+
+
+
+integer= 2
+string= 'ian'
+float = 2.4
+boolean = True
+list = []
+
+
+#camel case
+numberOfStudents=10
+
+# pascal 
+numberOfStudents=10
+# snake
+number_of_students=10
---
+++ LastPythonUnit.py/pythonReviewActivity.py
@@ -0,0 +1,9 @@
+1. # 'The operator you would use to perform calculations is the arthemitic operator, this operator is used because it is used to perform common math equations like 2+4 or 2x4, so the best operator would be arthemictic'
+
+# ' Assign operator would be a good operator to use, because you can assign someone the amount of money they owe if they are buying an item'
+
+2. # 'Comparison operator is the operator you would use in this situation, because you would compare the persons age in the verificatio nadn the age you have  to be see of they are able to get a ticket for the movie'
+# 'IF they are not of age they cant buy a ticket, but if they are 17 or older they can'
+
+3.  # 'If someone is making a reservation and they want to confirm their reservation. Assign operator would be the best operator to assign that person info to them and you could easily find that info.'
+

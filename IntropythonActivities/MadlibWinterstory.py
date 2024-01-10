69f55b9377c0f336bc337eef56637d186ecb5469
MGarrett14 <144712229+MGarrett14@users.noreply.github.com>
2024-01-10 17:52:53 +0000

    Madlib Winterbreak story finished.

---
+++ IntropythonActivities/MadlibWinterstory.py
@@ -0,0 +1,22 @@
+def Story():
+
+    
+    
+
+
+    print('It was a,' + adjective, 'winter morning, the morning of christmas! an exciting day for the boy, ' + noun ,'He was at his grandmother house wih his family and while everyone was sleeping, he was still awake, otp with his friends who lived in another,' + noun2 ,'but that didnt stop him from having fun with his best friends. ,' + noun , 'showed off his gifts to his friends, listening to their favorite band ,' + noun3 , 'cracking jokes while it was 3am they were still , ' + verb , 'feeling like the night could never end. A ,' + adjective2 , 'night.')
+print('Enter an adjective:')
+print('Enter another adjective:')
+print('Enter a noun:')
+print('Enter another noun')
+print('Enter a verb')
+print('Enter another verb')
+adjective= input()
+adjective2= input()
+noun= input()
+noun2= input()
+noun3= input()
+verb= input()
+verb2= input()
+
+Story()
---
+++ LastPythonUnit.py/asyncwork_jan8.py
@@ -0,0 +1,7 @@
+# reason 1 Programmers should write documentation to give instructions on how other programmers should use their code and what it can be used for.
+#         if a programmer made a good piece of code, other programmers can use that code to maybe create even better code and the documentation can help them figure out how to use the code and put it to good use.
+
+# reason 2 Programmers should write documentation to learn from mistakes you have made inside your code. 
+#         Making Documentation of your code could help you reflect on past mistake and when making an new piece of code you won't make those same mistake and posibly ruin other programmers codes'
+
+# The Final reason for programmers 
